4ffccc1898c9d01bb6d837fd5b62084a966e05c6
rickyx12 <aizekrysler@gmail.com>
2016-11-10 03:59:23 +0800

    1st commit

---
+++ ckeditor/config.js
@@ -0,0 +1,30 @@
+﻿/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+
+config.font_defaultLabel = 'Arial';
+config.fontSize_defaultLabel = '30px';
+config.contentsCss
+
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+
+//	config.extraPlugins = 'cursorpos';
+//	config.uiColor      = '#AADC6E';
+	
+	
+//	config.toolbar =
+//	[
+//		[ 'Source', '-', 'Bold','Italic','Underline' ]
+//	];
+	
+//	config.resize_enabled = false; // if you can resize
+//	config.width = 500; // starting width
+//	config.height = 600; // starting height
+
+
+}
