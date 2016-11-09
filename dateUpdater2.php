f895fcbf4869a97d3ce59966c1595110fc1408a3
rickyx12 <aizekrysler@gmail.com>
2016-11-10 04:10:30 +0800

    date updater for wrong format of date

---
+++ dateUpdater2.php
@@ -0,0 +1,71 @@
+<?php
+include("myDatabase.php");
+$day1 = $_POST['day1']; $controlDay1 = $_POST['controlDay1'];
+$day2 = $_POST['day2']; $controlDay2 = $_POST['controlDay2'];
+$day3 = $_POST['day3']; $controlDay3 = $_POST['controlDay3'];
+$day4 = $_POST['day4']; $controlDay4 = $_POST['controlDay4'];
+$day5 = $_POST['day5']; $controlDay5 = $_POST['controlDay5'];
+$day6 = $_POST['day6']; $controlDay6 = $_POST['controlDay6'];
+$day7 = $_POST['day7']; $controlDay7 = $_POST['controlDay7'];
+$day8 = $_POST['day8']; $controlDay8 = $_POST['controlDay8'];
+$day9 = $_POST['day9']; $controlDay9 = $_POST['controlDay9'];
+$day10 = $_POST['day10']; $controlDay10 = $_POST['controlDay10'];
+$day11 = $_POST['day11']; $controlDay11 = $_POST['controlDay11'];
+$day12 = $_POST['day12']; $controlDay12 = $_POST['controlDay12'];
+$day13 = $_POST['day13']; $controlDay13 = $_POST['controlDay13'];
+$day14 = $_POST['day14']; $controlDay14 = $_POST['controlDay14'];
+$day15 = $_POST['day15']; $controlDay15 = $_POST['controlDay15'];
+$day16 = $_POST['day16']; $controlDay16 = $_POST['controlDay16'];
+$day17 = $_POST['day17']; $controlDay17 = $_POST['controlDay17'];
+$day18 = $_POST['day18']; $controlDay18 = $_POST['controlDay18'];
+$day19 = $_POST['day19']; $controlDay19 = $_POST['controlDay19'];
+$day20 = $_POST['day20']; $controlDay20 = $_POST['controlDay20'];
+$day21 = $_POST['day21']; $controlDay21 = $_POST['controlDay21'];
+$day22 = $_POST['day22']; $controlDay22 = $_POST['controlDay22'];
+$day23 = $_POST['day23']; $controlDay23 = $_POST['controlDay23'];
+$day24 = $_POST['day24']; $controlDay24 = $_POST['controlDay24'];
+$day25 = $_POST['day25']; $controlDay25 = $_POST['controlDay25'];
+$day26 = $_POST['day26']; $controlDay26 = $_POST['controlDay26'];
+$day27 = $_POST['day27']; $controlDay27 = $_POST['controlDay27'];
+$day28 = $_POST['day28']; $controlDay28 = $_POST['controlDay28'];
+$day29 = $_POST['day29']; $controlDay29 = $_POST['controlDay29'];
+$day30 = $_POST['day30']; $controlDay30 = $_POST['controlDay30'];
+$day31 = $_POST['day31']; $controlDay31 = $_POST['controlDay31'];
+
+
+$ro = new database();
+
+//itp dpat and eedit
+$ro->editNow("registrationDetails","dateRegistered",$day1,"control_dateRegistered",$controlDay1);
+$ro->editNow("registrationDetails","dateRegistered",$day2,"control_dateRegistered",$controlDay2);
+$ro->editNow("registrationDetails","dateRegistered",$day3,"control_dateRegistered",$controlDay3);
+$ro->editNow("registrationDetails","dateRegistered",$day4,"control_dateRegistered",$controlDay4);
+$ro->editNow("registrationDetails","dateRegistered",$day5,"control_dateRegistered",$controlDay5);
+$ro->editNow("registrationDetails","dateRegistered",$day6,"control_dateRegistered",$controlDay6);
+$ro->editNow("registrationDetails","dateRegistered",$day7,"control_dateRegistered",$controlDay7);
+$ro->editNow("registrationDetails","dateRegistered",$day8,"control_dateRegistered",$controlDay8);
+$ro->editNow("registrationDetails","dateRegistered",$day9,"control_dateRegistered",$controlDay9);
+$ro->editNow("registrationDetails","dateRegistered",$day10,"control_dateRegistered",$controlDay10);
+$ro->editNow("registrationDetails","dateRegistered",$day11,"control_dateRegistered",$controlDay11);
+$ro->editNow("registrationDetails","dateRegistered",$day12,"control_dateRegistered",$controlDay12);
+$ro->editNow("registrationDetails","dateRegistered",$day13,"control_dateRegistered",$controlDay13);
+$ro->editNow("registrationDetails","dateRegistered",$day14,"control_dateRegistered",$controlDay14);
+$ro->editNow("registrationDetails","dateRegistered",$day15,"control_dateRegistered",$controlDay15);
+$ro->editNow("registrationDetails","dateRegistered",$day16,"control_dateRegistered",$controlDay16);
+$ro->editNow("registrationDetails","dateRegistered",$day17,"control_dateRegistered",$controlDay17);
+$ro->editNow("registrationDetails","dateRegistered",$day18,"control_dateRegistered",$controlDay18);
+$ro->editNow("registrationDetails","dateRegistered",$day19,"control_dateRegistered",$controlDay19);
+$ro->editNow("registrationDetails","dateRegistered",$day20,"control_dateRegistered",$controlDay20);
+$ro->editNow("registrationDetails","dateRegistered",$day21,"control_dateRegistered",$controlDay21);
+$ro->editNow("registrationDetails","dateRegistered",$day22,"control_dateRegistered",$controlDay22);
+$ro->editNow("registrationDetails","dateRegistered",$day23,"control_dateRegistered",$controlDay23);
+$ro->editNow("registrationDetails","dateRegistered",$day24,"control_dateRegistered",$controlDay24);
+$ro->editNow("registrationDetails","dateRegistered",$day25,"control_dateRegistered",$controlDay25);
+$ro->editNow("registrationDetails","dateRegistered",$day26,"control_dateRegistered",$controlDay26);
+$ro->editNow("registrationDetails","dateRegistered",$day27,"control_dateRegistered",$controlDay27);
+$ro->editNow("registrationDetails","dateRegistered",$day28,"control_dateRegistered",$controlDay28);
+$ro->editNow("registrationDetails","dateRegistered",$day29,"control_dateRegistered",$controlDay29);
+$ro->editNow("registrationDetails","dateRegistered",$day30,"control_dateRegistered",$controlDay30);
+$ro->editNow("registrationDetails","dateRegistered",$day31,"control_dateRegistered",$controlDay31);
+
+?>
