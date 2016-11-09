f66c4a4e2b415743f966be0eaa3a0ee010b23eca
rickyx12 <aizekrysler@gmail.com>
2016-11-10 03:49:16 +0800

    1st commit

---
+++ Maintenance/addCharges1.php
@@ -0,0 +1,21 @@
+<?php
+include("../myDatabase.php");
+$description = $_GET['description'];
+$services = $_GET['services'];
+$category = $_GET['category'];
+$opdprice = $_GET['opdprice'];
+$wardprice = $_GET['wardprice'];
+$solowardprice = $_GET['solowardprice'];
+$semiprivateprice = $_GET['semiprivateprice'];
+$privateprice = $_GET['privateprice'];
+$username = $_GET['username'];
+$subCategory = $_GET['subCategory'];
+$hmo = $_GET['hmo'];
+
+
+$ro = new database();
+
+$ro->addNewCharges($description,$services,$category,$opdprice,$wardprice,$solowardprice,$semiprivateprice,$privateprice,$username,$subCategory,$hmo);
+
+
+?>
