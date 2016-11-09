a9ac4486bcdfffd0a6e2cb6e6fd0499b9a254d7d
rickyx12 <aizekrysler@gmail.com>
2016-11-10 04:08:21 +0800

    date updater for wrong format of date

---
+++ dateUpdater.php
@@ -0,0 +1,43 @@
+
+
+<form method="post" action="dateUpdater1.php">
+<br><br><br>&nbsp;&nbsp;&nbsp;
+
+<select name="date_month">
+<option value="Jan">Jan</option>
+<option value="Feb">Feb</option>
+<option value="Mar">Mar</option>
+<option value="Apr">Apr</option>
+<option value="May">May</option>
+<option value="Jun">Jun</option>
+<option value="Jul">Jul</option>
+<option value="Aug">Aug</option>
+<option value="Sep">Sep</option>
+<option value="Oct">Oct</option>
+<option value="Nov">Nov</option>
+<option value="Dec">Dec</option>
+</select>
+
+
+<input type="text" name="date_year" value="">
+
+
+<hr>
+&nbsp;&nbsp;&nbsp;
+<select name="controlDate_month">
+<option value="01">Jan</option>
+<option value="02">Feb</option>
+<option value="03">Mar</option>
+<option value="04">Apr</option>
+<option value="05">May</option>
+<option value="06">Jun</option>
+<option value="07">Jul</option>
+<option value="08">Aug</option>
+<option value="09">Sep</option>
+<option value="10">Oct</option>
+<option value="11">Nov</option>
+<option value="12">Dec</option>
+</select>
+<Br><br>
+<input type="submit" value="Proceed">
+</form>
---
+++ dateUpdater1.php
@@ -0,0 +1,417 @@
+<?php
+$date_month = $_POST['date_month'];
+$date_year = $_POST['date_year'];
+$controlDate_month = $_POST['controlDate_month'];
+
+
+echo "<form method='post' action='dateUpdater2.php'>";
+echo "<Br><br>";
+
+/*********************************DAY 1*******************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_01_".$date_year."' checked=checked>
+<input type='text' name='day1' value='".$date_month."_01_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay1' value='".$date_year."-".$controlDate_month."-01'>
+<br>";
+/****************************************************************************************/
+
+
+/**********************************DAY 2******************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_02_".$date_year."' checked=1 >
+<input type='text' name='day2' value='".$date_month."_02_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay2' value='".$date_year."-".$controlDate_month."-02'>
+<br>";
+/****************************************************************************************/
+
+
+/***********************************DAY 3*****************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_03_".$date_year."' checked=1>
+<input type='text' name='day3' value='".$date_month."_03_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay3' value='".$date_year."-".$controlDate_month."-03'>
+<br>";
+/****************************************************************************************/
+
+
+
+/************************************DAY 4****************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_04_".$date_year."' checked=1 >
+<input type='text' name='day4' value='".$date_month."_04_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay4' value='".$date_year."-".$controlDate_month."-04'>
+<br>";
+/****************************************************************************************/
+
+
+
+/**************************************DAY 5**************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_05_".$date_year."' checked=1 >
+<input type='text' name='day5' value='".$date_month."_05_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay5' value='".$date_year."-".$controlDate_month."-05'>
+<br>";
+/****************************************************************************************/
+
+
+/*************************************DAY 6***************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_06_".$date_year."' checked=1 >
+<input type='text' name='day6' value='".$date_month."_06_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay6' value='".$date_year."-".$controlDate_month."-06'>
+<br>";
+/****************************************************************************************/
+
+
+
+/***************************************DAY 7*************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_07_".$date_year."' checked=1>
+<input type='text' name='day7' value='".$date_month."_07_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay7' value='".$date_year."-".$controlDate_month."-07'>
+<br>";
+/****************************************************************************************/
+
+
+
+/**************************************DAY 8**************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_08_".$date_year."' checked=1>
+<input type='text' name='day8' value='".$date_month."_08_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay8' value='".$date_year."-".$controlDate_month."-08'>
+<br>";
+/****************************************************************************************/
+
+
+
+
+/****************************************DAY 9************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_09_".$date_year."' checked=1 >
+<input type='text' name='day9' value='".$date_month."_09_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay9' value='".$date_year."-".$controlDate_month."-09'>
+<br>";
+/****************************************************************************************/
+
+
+
+/*****************************************DAY 10***********************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_10_".$date_year."' checked=1 >
+<input type='text' name='day10' value='".$date_month."_10_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay10' value='".$date_year."-".$controlDate_month."-10'>
+<br>";
+/****************************************************************************************/
+
+
+
+/*******************************************DAY 11*********************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_11_".$date_year."' checked=1>
+<input type='text' name='day11' value='".$date_month."_11_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay11' value='".$date_year."-".$controlDate_month."-11'>
+<br>";
+/****************************************************************************************/
+
+
+
+/*************************************DAY 12***************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_12_".$date_year."' checked=1>
+<input type='text' name='day12' value='".$date_month."_12_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay12' value='".$date_year."-".$controlDate_month."-12'>
+<br>";
+/****************************************************************************************/
+
+
+
+/**************************************DAY 13**************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_13_".$date_year."' checked=1>
+<input type='text' name='day13' value='".$date_month."_13_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay13' value='".$date_year."-".$controlDate_month."-13'>
+<br>";
+/****************************************************************************************/
+
+
+
+/**************************************DAY 14**************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_14_".$date_year."' checked=1>
+<input type='text' name='day14' value='".$date_month."_14_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay14' value='".$date_year."-".$controlDate_month."-14'>
+<br>";
+/****************************************************************************************/
+
+
+
+/**************************************DAY 15**************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_15_".$date_year."' checked=1>
+<input type='text' name='day15' value='".$date_month."_15_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay15' value='".$date_year."-".$controlDate_month."-15'>
+<br>";
+/****************************************************************************************/
+
+
+
+
+/***********************************DAY 16*****************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_16_".$date_year."' checked=1>
+<input type='text' name='day16' value='".$date_month."_16_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay16' value='".$date_year."-".$controlDate_month."-16'>
+<br>";
+/****************************************************************************************/
+
+
+
+
+/************************************DAY 17****************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_17_".$date_year."' checked=1>
+<input type='text' name='day17' value='".$date_month."_17_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay17' value='".$date_year."-".$controlDate_month."-17'>
+<br>";
+/****************************************************************************************/
+
+
+
+
+/**********************************DAY 18******************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_18_".$date_year."' checked=1>
+<input type='text' name='day18' value='".$date_month."_18_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay18' value='".$date_year."-".$controlDate_month."-18'>
+<br>";
+/****************************************************************************************/
+
+
+
+/***********************************DAY 19*****************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_19_".$date_year."' checked=1>
+<input type='text' name='day19' value='".$date_month."_19_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay19' value='".$date_year."-".$controlDate_month."-19'>
+<br>";
+/****************************************************************************************/
+
+
+
+/************************************DAY 20****************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_20_".$date_year."' checked=1>
+<input type='text' name='day20' value='".$date_month."_20_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay20' value='".$date_year."-".$controlDate_month."-20'>
+<br>";
+/****************************************************************************************/
+
+
+
+/*************************************DAY 21***************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_21_".$date_year."' checked=1>
+<input type='text' name='day21' value='".$date_month."_21_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay21' value='".$date_year."-".$controlDate_month."-21'>
+<br>";
+/****************************************************************************************/
+
+
+
+
+/*************************************DAY 22***************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_22_".$date_year."' checked=1>
+<input type='text' name='day22' value='".$date_month."_22_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay22' value='".$date_year."-".$controlDate_month."-22'>
+<br>";
+/****************************************************************************************/
+
+
+
+
+/*************************************DAY 23***************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_23_".$date_year."' checked=1>
+<input type='text' name='day23' value='".$date_month."_23_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay23' value='".$date_year."-".$controlDate_month."-23'>
+<br>";
+/****************************************************************************************/
+
+
+
+/************************************DAY 24****************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_24_".$date_year."' checked=1>
+<input type='text' name='day24' value='".$date_month."_24_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay24' value='".$date_year."-".$controlDate_month."-24'>
+<br>";
+/****************************************************************************************/
+
+
+
+
+/**************************************DAY 25**************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_25_".$date_year."' checked=1>
+<input type='text' name='day25' value='".$date_month."_25_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay25' value='".$date_year."-".$controlDate_month."-25'>
+<br>";
+/****************************************************************************************/
+
+
+
+
+/************************************DAY 26****************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_26_".$date_year."' checked=1>
+<input type='text' name='day26' value='".$date_month."_26_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay26' value='".$date_year."-".$controlDate_month."-26'>
+<br>";
+/****************************************************************************************/
+
+
+
+
+/*************************************DAY 27***************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_27_".$date_year."' checked=1>
+<input type='text' name='day27' value='".$date_month."_27_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay27' value='".$date_year."-".$controlDate_month."-27'>
+<br>";
+/****************************************************************************************/
+
+
+
+/**************************************DAY 28**************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_28_".$date_year."' checked=1>
+<input type='text' name='day28' value='".$date_month."_28_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay28' value='".$date_year."-".$controlDate_month."-28'>
+<br>";
+/****************************************************************************************/
+
+
+
+/***************************************DAY 29*************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_29_".$date_year."' checked=1>
+<input type='text' name='day29' value='".$date_month."_29_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay29' value='".$date_year."-".$controlDate_month."-29'>
+<br>";
+/****************************************************************************************/
+
+
+/****************************************DAY 30************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_30_".$date_year."' checked=1>
+<input type='text' name='day30' value='".$date_month."_30_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay30' value='".$date_year."-".$controlDate_month."-30'>
+<br>";
+/****************************************************************************************/
+
+
+/***************************************DAY 31*************************************************/
+echo "
+<input type='checkbox' name='checkbox' value='".$date_month."_31_".$date_year."' checked=1>
+<input type='text' name='day31' value='".$date_month."_31_".$date_year."' >
+&nbsp;
+---->
+&nbsp;
+<input type='text' name='controlDay31' value='".$date_year."-".$controlDate_month."-31'>
+<br>";
+/****************************************************************************************/
+echo "<br><br><Br><input type='submit' value='Proceed'>";
+echo "</form>";
+
+?>
